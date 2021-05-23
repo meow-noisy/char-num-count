e62ee2178d765af4d6b41c4db23b125a94f6c1f7
meow <meow.noisy.code@gmail.com>
2021-05-23 13:16:19 +0900

    fix logic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 // 置き換わるくさい
 <template>
-  <div>
-    <p>{{ countCharNum }}</p>
-    <input v-model="message" />
-  </div>
+  <v-app>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>char num count</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <p>{{ countCharNum }}</p>
+              <v-text-field v-model="message" />
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
